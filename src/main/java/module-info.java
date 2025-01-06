297324b4796478f7c34d64a6339fad2283408dbf
Nurullah K <nurullahkaratas@gmail.com>
2025-01-07 00:02:54 +0100

    Created Readme file over the project

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -6,6 +6,7 @@ module com.example.rongorongo {
     requires com.dlsc.formsfx;
     requires org.kordamp.ikonli.javafx;
     requires org.kordamp.bootstrapfx.core;
+    requires java.desktop;
 
     opens com.example.rongorongo to javafx.fxml;
     exports com.example.rongorongo;
